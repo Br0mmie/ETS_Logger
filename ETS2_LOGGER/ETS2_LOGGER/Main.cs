998ddf66a28acd4ed9139c77fe956a84c6b67145
SHBProductions <stihaabr@gmail.com>
2018-01-04 20:43:56 +0100

    Main Commit
    Main Commit

---
+++ ETS2_LOGGER/ETS2_LOGGER/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EtsLogger;
+
+namespace EtsLogger
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public void GetData()
+        {
+
+        }
+    }
+}
